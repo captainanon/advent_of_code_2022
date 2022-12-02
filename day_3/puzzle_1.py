b2e2a0810c4999cf454f0f58f94bf6a8454a504e
Captain Anonymous <captainanon15@gmail.com>
2022-12-02 12:10:38 -0700

    add requirements.txt file

---
+++ day_3/puzzle_1.py
@@ -0,0 +1,16 @@
+# https://adventofcode.com/2022/day/3
+
+
+def process_stragety_guide(file):
+    pass
+        
+
+def get_score(file):
+    pass
+
+    
+if __name__ == '__main__':
+    test_inputs = 'puzzle_test_inputs.txt'
+    actual_inpus = 'puzzle_inputs.txt'
+    print(get_score(test_inputs)) # 
+    print(get_score(actual_inpus)) # 
