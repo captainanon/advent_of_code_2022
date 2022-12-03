d83abd5e582a635442e3deafac278325999a102e
Captain Anonymous <captainanon15@gmail.com>
2022-12-03 07:01:46 -0700

    add day 3 puzzles

--- day_3/puzzle_1.py
+++ day_3/puzzle_1.py
@@ -1,16 +1,45 @@
 # https://adventofcode.com/2022/day/3
 
 
-def process_stragety_guide(file):
-    pass
-        
+def process_puzzle_inputs(file):
+    inputs = []
+    with open(file, 'r') as f:
+        for line in f:
+            inputs.append(line.strip('\n'))
+    return inputs
 
-def get_score(file):
-    pass
+def solve_puzzle(file):
+    inputs = process_puzzle_inputs(file)
+    alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    scores = dict(zip(alpha, list(range(1, 53))))   
+    sum = 0
+    for input in inputs:
+        ruck_sack_1_items_count = dict(zip(alpha, ([0] * 52).copy()))
+        ruck_sack_2_items_count = dict(zip(alpha, ([0] * 52).copy()))
+        num_ruck_sack_items = int(len(input) / 2)
+        ruck_sack_1 = input[:num_ruck_sack_items]
+        ruck_sack_2 = input[num_ruck_sack_items:]
+        item = ''
+        i = 0
+        while i < num_ruck_sack_items:
+            ruck_sack_1_item = ruck_sack_1[i]
+            ruck_sack_1_items_count[ruck_sack_1_item] += 1
+            if ruck_sack_1_items_count[ruck_sack_1_item] > 0 and ruck_sack_2_items_count[ruck_sack_1[i]] > 0:
+                item = ruck_sack_1_item
+                break
+
+            ruck_sack_2_item = ruck_sack_2[i]
+            ruck_sack_2_items_count[ruck_sack_2_item] += 1
+            if ruck_sack_2_items_count[ruck_sack_2_item] > 0 and ruck_sack_1_items_count[ruck_sack_2[i]] > 0:
+                item = ruck_sack_2_item
+                break
+            i += 1
+        sum += scores[item]
+    return sum
 
     
 if __name__ == '__main__':
     test_inputs = 'puzzle_test_inputs.txt'
     actual_inpus = 'puzzle_inputs.txt'
-    print(get_score(test_inputs)) # 
-    print(get_score(actual_inpus)) # 
+    print(solve_puzzle(test_inputs)) # 157
+    print(solve_puzzle(actual_inpus)) # 7793
--- day_3/puzzle_2.py
+++ day_3/puzzle_2.py
@@ -1,16 +1,46 @@
 # https://adventofcode.com/2022/day/3#part2
 
 
-def process_stragety_guide(file):
-    pass
-        
+def process_puzzle_inputs(file):
+    inputs = []
+    tup_list = []
+    with open(file, 'r') as f:
+        for line in f:
+            inputs.append(line.strip('\n'))
+        for i in range(0, len(inputs)-2, 3):
+            tup_list.append((inputs[i], inputs[i+1], inputs[i+2]))
+    return tup_list
 
-def get_score(file):
-    pass
+def solve_puzzle(file):
+    inputs = process_puzzle_inputs(file)
+    alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    scores = dict(zip(alpha, list(range(1, 53))))   
+    sum = 0
+    for input in inputs:
+        ruck_sack_1_items_count = dict(zip(alpha, ([0] * 52).copy()))
+        ruck_sack_2_items_count = dict(zip(alpha, ([0] * 52).copy()))
+        ruck_sack_3_items_count = dict(zip(alpha, ([0] * 52).copy()))
+        badge = ''
+        for item in input[0]:
+            ruck_sack_1_items_count[item] += 1
+            if ruck_sack_1_items_count[item] > 0 and ruck_sack_2_items_count[item] > 0 and ruck_sack_3_items_count[item] > 0:
+                badge = item
+                break
+        for item in input[1]:
+            ruck_sack_2_items_count[item] += 1
+            if ruck_sack_1_items_count[item] > 0 and ruck_sack_2_items_count[item] > 0 and ruck_sack_3_items_count[item] > 0:
+                badge = item
+                break
+        for item in input[2]:
+            ruck_sack_3_items_count[item] += 1
+            if ruck_sack_1_items_count[item] > 0 and ruck_sack_2_items_count[item] > 0 and ruck_sack_3_items_count[item] > 0:
+                badge = item
+                break
+        sum += scores[badge]
+    return sum
 
-    
 if __name__ == '__main__':
     test_inputs = 'puzzle_test_inputs.txt'
     actual_inpus = 'puzzle_inputs.txt'
-    print(get_score(test_inputs)) # 
-    print(get_score(actual_inpus)) # 
+    print(solve_puzzle(test_inputs)) # 70
+    print(solve_puzzle(actual_inpus)) # 2499
