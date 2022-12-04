788abc11079fad8522e95280d1db23e451ce8758
Captain Anonymous <captainanon15@gmail.com>
2022-12-04 05:59:37 -0700

    add day 4 completed puzzles

--- day_4/puzzle_1.py
+++ day_4/puzzle_1.py
@@ -5,17 +5,30 @@ def process_puzzle_inputs(file):
     inputs = []
     with open(file, 'r') as f:
         for line in f:
-            inputs.append(line.strip('\n'))
+            step1 = line.strip('\n')
+            step2 = step1.split(',')
+            step3 = [x.split('-') for x in step2]
+            step4a = (int(step3[0][0]), int(step3[0][1]))
+            step4b = (int(step3[1][0]), int(step3[1][1]))
+            inputs.append((step4a, step4b))
     return inputs
 
 
 def solve_puzzle(file):
-    pass
+    elves = process_puzzle_inputs(file)
+    count = 0
+    for pair in elves:
+        set1 = set(range(pair[0][0], pair[0][1] + 1))
+        set2 = set(range(pair[1][0], pair[1][1] + 1))
+        intersection = set1.intersection(set2)
+        if  intersection == set1 or intersection == set2:
+            count +=1
+    return count  
 
     
 if __name__ == '__main__':
     test_inputs = 'puzzle_test_inputs.txt'
-    print(solve_puzzle(test_inputs)) # 
+    print(solve_puzzle(test_inputs)) 
 
-    # actual_inpus = 'puzzle_inputs.txt'
-    # print(solve_puzzle(actual_inpus)) # 
+    actual_inpus = 'puzzle_inputs.txt' # 2
+    print(solve_puzzle(actual_inpus)) # 500
--- day_4/puzzle_2.py
+++ day_4/puzzle_2.py
@@ -5,17 +5,30 @@ def process_puzzle_inputs(file):
     inputs = []
     with open(file, 'r') as f:
         for line in f:
-            inputs.append(line.strip('\n'))
+            step1 = line.strip('\n')
+            step2 = step1.split(',')
+            step3 = [x.split('-') for x in step2]
+            step4a = (int(step3[0][0]), int(step3[0][1]))
+            step4b = (int(step3[1][0]), int(step3[1][1]))
+            inputs.append((step4a, step4b))
     return inputs
-    
+
 
 def solve_puzzle(file):
-    pass
+    elves = process_puzzle_inputs(file)
+    count = 0
+    for pair in elves:
+        set1 = set(range(pair[0][0], pair[0][1] + 1))
+        set2 = set(range(pair[1][0], pair[1][1] + 1))
+        num = len(set1.intersection(set2))
+        if  num > 0:
+            count +=1
+    return count  
 
     
 if __name__ == '__main__':
     test_inputs = 'puzzle_test_inputs.txt'
-    print(solve_puzzle(test_inputs)) # 
+    print(solve_puzzle(test_inputs)) 
 
-    # actual_inpus = 'puzzle_inputs.txt'
-    # print(solve_puzzle(actual_inpus)) # 
+    actual_inpus = 'puzzle_inputs.txt' # 4
+    print(solve_puzzle(actual_inpus)) # 815
